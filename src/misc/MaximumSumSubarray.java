6fd2e25176c825dd3b475b193d3f37a23805b1b8
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-09-24 10:51:03 -0400

    MSS

--- src/misc/MaximumSumSubarray.java
+++ src/misc/MaximumSumSubarray.java
@@ -4,60 +4,32 @@ import java.util.Arrays;
 
 public class MaximumSumSubarray {
     
-    public static int naive(int[] array) {
-        
-        int max = 0;
-        
-        for(int start = 0; start < array.length - 1; start++) {
-            for(int end = 1; end < array.length; end++) {
-                int sum = 0;
-                for(int i = start; i <= end; i++) {
-                    sum += array[i];
-                }
-                if(sum > max) {
-                    max = sum;
-                }
-            }
+    public static int dynamic(int[] A) {
+        int[] memo = new int[A.length + 1];
+        memo[0] = 0;
+        for(int i = 1; i < memo.length; i++) {
+            memo[i] = Math.max(memo[i - 1] + A[i - 1], 0);
         }
-        
+        int max = 0;
+        for(int i = 0; i < memo.length; i++) {
+            max = Math.max(memo[i], max);
+        }
         return max;
     }
     
-    public static int dynamic(int[] array) {
-        int[] maxes = new int[array.length];
-        
-        maxes[0] = Math.max(array[0], 0);
-        
-        for(int i = 1; i < array.length; i++) {
-            maxes[i] = Math.max(maxes[i-1] + array[i], 0);
-        }
-        
-        int max = 0;
-        
-        for(int i = 0; i < maxes.length; i++) {
-            if(max < maxes[i]) {
-                max = maxes[i];
-            }
-        }
-        
-        return max;
-    }
-    
-    public static int smart(int[] array) {
+    public static int smart(int[] A) {
         int current_max_so_far = 0;
-        int best_max_so_far = 0;
-        
-        for(int i = 0; i < array.length; i++) {
-            current_max_so_far = Math.max(current_max_so_far + array[i], 0);
-            best_max_so_far = Math.max(current_max_so_far, best_max_so_far);
+        int max_so_far = 0;
+        for(int i = 0; i < A.length; i++) {
+            current_max_so_far = Math.max(current_max_so_far + A[i], 0);
+            max_so_far = Math.max(current_max_so_far, max_so_far);
         }
-        
-        return best_max_so_far;
+        return max_so_far;
     }
-
+    
     public static void main(String[] args) {
-        int[] array = {-3, -6, 10, 3, -1, 10};
-        System.out.println(naive(array));
+        int[] array = {-1, 4, -2, 0, 6, 1};
+//        System.out.println(naive(array));
         System.out.println(dynamic(array));
         System.out.println(smart(array));
     }
