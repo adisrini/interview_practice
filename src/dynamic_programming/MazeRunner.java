2d619001d569b5635929f19249d0c13c2078f215
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-09-30 01:52:57 -0400

    DP maze runner

--- src/dynamic_programming/MazeRunner.java
+++ src/dynamic_programming/MazeRunner.java
@@ -2,7 +2,7 @@ package dynamic_programming;
 
 public class MazeRunner {
     
-    public static int ways(int[][] grid) {
+    public static int ways_r(int[][] grid) {
         return ways(grid, 0, 0);
     }
     
@@ -16,12 +16,50 @@ public class MazeRunner {
         return ways(grid, r + 1, c) + ways(grid, r, c + 1);
     }
     
+    public static int ways_i(int[][] grid) {
+        int[][] memo = new int[grid.length][grid[0].length];
+        
+        int row = memo.length - 1;
+        boolean blocked = false;
+        for(int col = memo[0].length - 1; col >= 0; col--) {     // initialize bottom row
+            if(grid[row][col] == 1) {
+                blocked = true;
+            } else {
+                if(!blocked) {
+                    memo[row][col] = 1;
+                } else {
+                    memo[row][col] = 0;
+                }
+            }
+        }
+        
+        for(int r = memo.length - 2; r >= 0; r--) {
+            for(int c = memo[r].length - 1; c >= 0; c--) {
+                if(grid[r][c] != 1) {
+                    int right = (c == memo[r].length - 1) ? 0 : memo[r][c + 1];
+                    int bottom = memo[r + 1][c];
+                    memo[r][c] = right + bottom;
+                }
+            }
+        }
+        
+        return memo[0][0];
+        
+    }
+    
     public static void main(String[] args) {
-        int[][] grid = new int[][]{{0, 0, 1},
-                                   {0, 1, 0},
-                                   {0, 0, 0},};
-        int ways = ways(grid);
-        System.out.println(ways);
+        int[][] grid = new int[][]{{0, 0, 0, 0, 0, 0, 0, 0},
+                                   {0, 0, 0, 0, 1, 0, 0, 0},
+                                   {1, 0, 0, 0, 0, 0, 1, 0},
+                                   {0, 0, 0, 0, 0, 1, 0, 0},
+                                   {0, 0, 1, 1, 0, 0, 0, 0},
+                                   {0, 0, 0, 0, 0, 0, 0, 0},
+                                   {0, 1, 0, 0, 1, 0, 1, 0},
+                                   {0, 0, 0, 0, 0, 0, 0, 0}};
+        int ways_r = ways_r(grid);
+        int ways_m = ways_i(grid);
+        System.out.println(ways_r);
+        System.out.println(ways_m);
     }
 
 }
